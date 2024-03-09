b199e419d18b2b743989e689d21969db45bad16f
Pradyumna173 <prponkshe173@gmail.com>
2024-03-09 18:27:45 +0530

    Added Neotree Config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,5 +3,6 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  require "custom.plugins.toggleterm"
+  require "custom.plugins.toggleterm",
+  require "custom.plugins.neotree"
 }
